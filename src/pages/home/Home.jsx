b01568c001ad5fd905456b6e5e499ccdcac5588e
taibislamdipu <mailtaibislam@gmail.com>
2023-02-10 02:16:18 +0600

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,16 +4,20 @@ import Navbar from "../../layout/Navbar";
 import MeetTheCast from "./MeetTheCast";
 import Episodes from "./Episodes";
 import Locations from "./Locations";
+import portal from "../../assets/home-page/hero-elements/portal.png";
+import bubble from "../../assets/home-page/hero-elements/bubble.png";
 
 const Home = () => {
   return (
     <div className="home-container h-max">
       <Navbar />
       <section className="text-center">
-        <h1 className="font-bodyFont uppercase">
-          <span className="italic">The</span> Rick & Morty{" "}
-          <span className="italic">Wiki</span>
+        <h1 className="hero-text font-bodyFont uppercase">
+          <span className="top-left testb italic">The</span>
+          <img src={portal} alt="portal" />
+          Rick & Morty <span className="italic">Wiki</span>
         </h1>
+
         <div className="flex justify-center gap-2">
           <button className="btn">Watch Now</button>
           <p className="w-1/4">
