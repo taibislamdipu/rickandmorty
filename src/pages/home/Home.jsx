015224025afcc8376c3405bf84a1be7ac26b08cb
taibislamdipu <mailtaibislam@gmail.com>
2023-02-13 17:31:34 +0600

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,37 +12,10 @@ const Home = () => {
   return (
     <div className="home-container h-fit">
       <Navbar />
-      {/* <section className="mb-20 justify-between">
-        <h1 className="text-container container mx-auto flex flex-wrap items-center justify-center gap-2 font-heroFont">
-          <span className="text neonText uppercase italic" />
-          <img src={portal} alt="" className="neonText" />
-          <span className="neonText bg-gradient-to-r from-secondary to-thirdColor bg-clip-text uppercase text-transparent">
-            Rick <span className="and" />
-          </span>
-          <span className="bg-gradient-to-r from-secondary to-thirdColor bg-clip-text uppercase text-transparent">
-            Morty
-          </span>
-          <span className="italic">Wiki</span>
-        </h1>
-        <span></span>
-
-        <div className="flex items-center justify-center gap-10">
-          <button className="btn gap-4 rounded-full bg-gradient-to-r from-secondary to-thirdColor">
-            <AiFillPlayCircle size={23} />
-            Watch Now
-          </button>
-          <p className="w-1/4 text-thirdColor">
-            Brilliant but boozy scientist Rick hijacks his fretful teenage
-            grandson, Morty, for wild escapades in other worlds and alternate
-            dimensions.
-          </p>
-        </div>
-      </section> */}
-
-      <section className="hero-text-container mb-20 flex items-center px-10">
+      <section className="mb-20 flex items-center px-10">
         <div className="flex w-full md:justify-center md:text-center">
-          <div className=" z-10 space-y-4">
-            <h1 className="space-x-2 font-heroFont">
+          <div className="z-10 space-y-4">
+            <h1 className="space-x-2 font-heroFont font-extrabold">
               <span className="neonText the-text italic">The</span>
               <img src={portal} className="inline w-20 md:w-min" alt="" />
               <span className="hero-gradient-text">
