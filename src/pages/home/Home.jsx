dbf80bfbc26f03ca8c89c369ef08980b15907ffe
taibislamdipu <mailtaibislam@gmail.com>
2023-02-13 17:03:14 +0600

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,13 +5,14 @@ import MeetTheCast from "./MeetTheCast";
 import Episodes from "./Episodes";
 import Locations from "./Locations";
 import portal from "../../assets/home-page/hero-elements/portal.png";
+import gun from "../../assets/home-page/hero-elements/gun.png";
 import { AiFillPlayCircle } from "react-icons/ai";
 
 const Home = () => {
   return (
     <div className="home-container h-fit">
       <Navbar />
-      <section className="mb-20 justify-between">
+      {/* <section className="mb-20 justify-between">
         <h1 className="text-container container mx-auto flex flex-wrap items-center justify-center gap-2 font-heroFont">
           <span className="text neonText uppercase italic" />
           <img src={portal} alt="" className="neonText" />
@@ -36,7 +37,39 @@ const Home = () => {
             dimensions.
           </p>
         </div>
+      </section> */}
+
+      <section className="hero-text-container mb-20 flex items-center px-10">
+        <div className="flex w-full md:justify-center md:text-center">
+          <div className=" z-10 space-y-4">
+            <h1 className="space-x-2 font-heroFont">
+              <span className="neonText the-text italic">The</span>
+              <img src={portal} className="inline w-20 md:w-min" alt="" />
+              <span className="hero-gradient-text">
+                Rick &
+                <br />
+                Morty
+              </span>
+              <span className="italic">Wiki</span>
+            </h1>
+            <div className="flex-row-reverse items-center justify-center space-y-4 md:flex md:space-y-0">
+              <p className="max-w-md text-thirdColor">
+                Brilliant but boozy scientist Rick hijacks his fretful teenage
+                grandson, Morty, for wild escapades in other worlds and
+                alternate dimensions.
+              </p>
+              <button className="btn gap-4 rounded-full bg-gradient-to-r from-secondary to-thirdColor">
+                <AiFillPlayCircle size={23} />
+                Watch Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute right-0">
+          <img src={gun} className="w-52 md:w-full" alt="" />
+        </div>
       </section>
+
       <section className="home-rest-content space-y-10 pb-40">
         <MeetTheCast />
         <div className="episode-and-location space-y-10 ">
